0ab9acce0821a878549da62f530773919b5eab21
vijay1234455 <90520645+vijay1234455@users.noreply.github.com>
2021-10-15 17:32:53 +0530

    Update datatypes.py

--- datatypes/datatypes.py
+++ datatypes/datatypes.py
@@ -1,5 +1,7 @@
 
-n=input("Enter value")
+n=input("Enter value")  #here input function takes the input from the user and it converts it into string type
 
-print("value entered:",n)
-print("type of value you entered",type(n))
+print("value entered:",n)  #it will print the value is given by the user
+print("type of value you entered",type(n))  # it will shows the type of the input value,here it is string type
+
+
